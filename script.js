13de604b4f0656acd50d979203404cb5a15af922
shop0129 <shop0129@gmail.com>
2026-06-26 02:08:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+// ==========================
+// 頁面切換
+// ==========================
 
+function showPage(pageId){
+
+    document.querySelectorAll(".page").forEach(page=>{
+
+        page.style.display="none";
+
+    });
+
+    document.getElementById(pageId).style.display="flex";
+
+}
+
+// ==========================
+// 首頁
+// ==========================
+
+document.getElementById("startBtn").onclick=function(){
+
+    showPage("ticketPage");
+
+}
+
+// ==========================
+// 返回首頁
+// ==========================
+
+document.getElementById("backBtn").onclick=function(){
+
+    showPage("homePage");
+
+}
+
+// ==========================
+// 啟動
+// ==========================
+
+showPage("homePage");
