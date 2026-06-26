16c002c4a78eb819f95bfd55263f39f271789d90
shop0129 <shop0129@gmail.com>
2026-06-26 11:16:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -167,9 +167,9 @@ function updateSuccessItems(){
 
         case "token,band,toy":
 
-            html += "<div>🪙 請領取代幣</div>";
-            html += "<div>🎫 請領取入場手環</div>";
-            html += "<div>🎁 玩具請於離場時憑手環兌換</div>";
+            html += "<div>🪙 請領取代幣🪙</div>";
+            html += "<div>🎫 請領取入場手環🎫 </div>";
+            html += "<div>🎁 玩具請於離場時憑手環兌換🎁 </div>";
             break;
 
         case "band":
