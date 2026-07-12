2390d0088673d6885b21a13606c6b4cc5d5c3825
shop0129 <shop0129@gmail.com>
2026-07-12 20:44:53 +0800

    Update history.js

--- js/modules/history.js
+++ js/modules/history.js
@@ -319,3 +319,28 @@ function cancelOrder(orderNo){
 
     order.status = "cancel";
 
+order.items.forEach(item=>{
+
+    rollbackStats(todayStats, item, item);
+
+    rollbackStats(monthStats, item, item);
+
+    rollbackStats(totalStats, item, item);
+
+});
+
+// 儲存統計
+saveTodayStats();
+
+// 儲存售票紀錄
+saveSalesHistory();
+
+alert("✅ 訂單已作廢");
+
+    openOrderDetail(
+        salesHistory.findIndex(
+            x => x.orderNo === orderNo
+        )
+    );
+
+}
