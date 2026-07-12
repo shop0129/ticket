524f09434cbfffbea55003139623396861e0cce3
shop0129 <shop0129@gmail.com>
2026-07-12 11:46:23 +0800

    Update page.js

--- js/modules/page.js
+++ js/modules/page.js
@@ -1,1 +1,62 @@
+function showPage(pageId){
 
+    clearInterval(countdownTimer);
+
+    document.querySelectorAll(".page").forEach(page=>{
+
+        page.classList.remove("active");
+
+    });
+
+    document
+        .getElementById(pageId)
+        .classList.add("active");
+
+    resetIdleTimer();
+
+}
+function resetIdleTimer(){
+
+    clearTimeout(idleTimer);
+
+    idleTimer = setTimeout(()=>{
+
+        // 如果不是首頁就回首頁
+        if(!document.getElementById("homePage").classList.contains("active")){
+
+            showPage("homePage");
+
+        }
+
+    },60000);
+
+}
+document.addEventListener("click", resetIdleTimer);
+
+document.addEventListener("touchstart", resetIdleTimer);
+document
+.getElementById("startBtn")
+.addEventListener("click",()=>{
+
+    playClick();
+
+    setTimeout(()=>{
+
+        showPage("ticketPage");
+
+    },100);
+
+});
+document
+.getElementById("backBtn")
+.addEventListener("click",()=>{
+
+    playClick();
+
+    setTimeout(()=>{
+
+        showPage("homePage");
+
+    },80);
+
+});
