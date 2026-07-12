50ec8afb0c246450664cc6844964adf9227ebc4c
shop0129 <shop0129@gmail.com>
2026-07-12 11:37:14 +0800

    Update sound.js

--- js/utils/sound.js
+++ js/utils/sound.js
@@ -1,1 +1,29 @@
+// =========================================
+// Monster Ticket V5
+// sound.js
+// 音效
+// =========================================
 
+function playClick(){
+
+    const click = document.getElementById("clickSound");
+
+    click.pause();
+
+    click.currentTime = 0;
+
+    click.play().catch(()=>{});
+
+}
+
+function playSuccess(){
+
+    const sound = document.getElementById("successSound");
+
+    sound.pause();
+
+    sound.currentTime = 0;
+
+    sound.play().catch(()=>{});
+
+}
