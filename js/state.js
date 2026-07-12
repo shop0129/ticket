8a3d430a8d11606d2ae86e0e71ede4a57c5b0a2b
shop0129 <shop0129@gmail.com>
2026-07-12 09:48:24 +0800

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,65 @@
+// =========================================
+// 小怪獸售票機 V5
+// state.js
+// 系統狀態
+// =========================================
+let cart = [];
 
+// 目前列印中的訂單
+let currentPrintOrder = null;
+
+// 是否為補印模式
+let isReprint = false;
+
+// 閒置計時
+let idleTimer;
+let countdownTimer;
+// 目前選擇票券
+let selectedReward = "";
+let selectedTicket = "";
+let todayStats = JSON.parse(
+    localStorage.getItem("todayStats")
+) || {
+
+    tickets:0,
+
+    income:0,
+
+    tokens:0,
+
+    greenToy:0,
+
+    redToy:0,
+
+    parent:0
+
+};
+let monthStats = JSON.parse(
+    localStorage.getItem("monthStats")
+) || {
+
+    tickets:0,
+    income:0,
+    tokens:0,
+    greenToy:0,
+    redToy:0,
+    parent:0
+
+};
+let totalStats = JSON.parse(
+    localStorage.getItem("totalStats")
+) || {
+
+    tickets:0,
+    income:0,
+    tokens:0,
+    greenToy:0,
+    redToy:0,
+    parent:0
+
+};
+let salesHistory = JSON.parse(
+
+    localStorage.getItem("salesHistory")
+
+) || [];
