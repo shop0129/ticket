a5c662639fab80de51924c6ab204ad2954d7e73e
shop0129 <shop0129@gmail.com>
2026-07-12 12:01:51 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1383,62 +1383,10 @@ function renderTicketManager(){
    V3.9 購物車
 ========================================= */
 
-function addCurrentTicketToCart(){
-
-    if(!selectedTicket) return;
-
-    const ticket = ticketData[selectedTicket];
-
-    cart.push({
-
-        id:selectedTicket,
-
-        title:ticket.title,
-
-        price:ticket.price,
-
-        token:ticket.token || 0,
-
-        toy:ticket.toy || "none",
-
-        reward:ticket.reward || ""
-
-    });
-
-    updateCartPanel();
-
-    showPage("ticketPage");
-
-}
 
 
-function updateCartPanel(){
 
-    const count=document.getElementById("cartCount");
-    const items=document.getElementById("cartItems");
-    const amount=document.getElementById("cartAmount");
 
-    let total=0;
-
-    const summary={};
-
-    cart.forEach(ticket=>{
-
-        total+=ticket.price;
-
-        if(!summary[ticket.id]){
-
-            summary[ticket.id]={
-
-                title:ticket.title,
-
-                qty:0,
-
-                amount:0
-
-            };
-
-        }
 
         summary[ticket.id].qty++;
 
@@ -1534,61 +1482,12 @@ function updateCartPanel(){
 }
 
 
-function changeQty(id,step){
 
-    if(step>0){
-
-        const ticket=ticketData[id];
-
-        cart.push({
-
-            id:id,
-
-            title:ticket.title,
-
-            price:ticket.price,
-
-            token:ticket.token||0,
-
-            toy:ticket.toy||"none",
-
-            reward:ticket.reward||""
-
-        });
-
-    }else{
-
-        const index=cart.findIndex(x=>x.id===id);
-
-        if(index!=-1){
-
-            cart.splice(index,1);
-
-        }
-
-    }
-
-    updateCartPanel();
-
-}
-
-
-function removeCartItem(id){
-
-    cart=cart.filter(x=>x.id!==id);
-
-    updateCartPanel();
-
-}
 // =========================================
 // V3.9.9 購物車付款
 // =========================================
 
-const checkoutBtn = document.getElementById("checkoutBtn");
-const paymentArea = document.getElementById("paymentArea");
-const cartLineBtn = document.getElementById("cartLineBtn");
-const cartCashBtn = document.getElementById("cartCashBtn");
-const cartBackBtn = document.getElementById("cartBackBtn");
+
 
 console.log("checkoutBtn =", checkoutBtn);
 console.log("paymentArea =", paymentArea);
@@ -1596,36 +1495,12 @@ console.log("cartLineBtn =", cartLineBtn);
 console.log("cartCashBtn =", cartCashBtn);
 console.log("cartBackBtn =", cartBackBtn);
 
-checkoutBtn.addEventListener("click",()=>{
 
-    if(cart.length===0){
-
-        alert("請先加入票券！");
-
-        return;
-
-    }
-
-    checkoutBtn.style.display="none";
-
-    paymentArea.style.display="flex";
-
-});
-
-cartBackBtn.addEventListener("click",()=>{
-
-    paymentArea.style.display="none";
-
-    checkoutBtn.style.display="block";
-
-});
 // =========================
 // 購物車付款
 // =========================
 
-bindPaymentButton(cartLineBtn,"LINE Pay");
 
-bindPaymentButton(cartCashBtn,"現金");
 const todayTab = document.getElementById("todayTab");
 const monthTab = document.getElementById("monthTab");
 const totalTab = document.getElementById("totalTab");
