12ca04dcd3c26b92f067d8524530e42513fbf711
shop0129 <shop0129@gmail.com>
2026-07-12 11:24:10 +0800

    Update helper.js

--- js/utils/helper.js
+++ js/utils/helper.js
@@ -1,1 +1,35 @@
+function generateOrderNo(){
 
+    let orderIndex = Number(localStorage.getItem("orderIndex")) || 1;
+
+    const now = new Date();
+
+    const y = now.getFullYear();
+
+    const m = String(now.getMonth()+1).padStart(2,"0");
+
+    const d = String(now.getDate()).padStart(2,"0");
+
+    const orderNo =
+        "M" +
+        y +
+        m +
+        d +
+        String(orderIndex).padStart(4,"0");
+
+    localStorage.setItem("orderIndex", orderIndex + 1);
+
+    return orderNo;
+
+}
+function playClick(){
+
+    const click = document.getElementById("clickSound");
+
+    click.pause();
+
+    click.currentTime = 0;
+
+    click.play().catch(function(){});
+
+}
