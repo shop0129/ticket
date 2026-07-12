acab9f61143f73c173c7dec8fcdc9c6811fdb7c4
shop0129 <shop0129@gmail.com>
2026-07-12 11:16:56 +0800

    Create storage.js

---
+++ js/utils/storage.js
@@ -0,0 +1,18 @@
+function saveTodayStats(){
+
+    localStorage.setItem(
+        "todayStats",
+        JSON.stringify(todayStats)
+    );
+
+    localStorage.setItem(
+        "monthStats",
+        JSON.stringify(monthStats)
+    );
+
+    localStorage.setItem(
+        "totalStats",
+        JSON.stringify(totalStats)
+    );
+
+}
