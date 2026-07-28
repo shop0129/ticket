a5e7d5a2e00627fa193e9339f7a5d3c9e731636e
shop0129 <shop0129@gmail.com>
2026-07-28 16:05:51 +0800

    Delete 01_TicketKiosk_Web_V7.8.3.3_Sprint6 directory

--- 01_TicketKiosk_Web_V7.8.3.3_Sprint6/tests/ticket-data-sync-v771.test.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs'),vm=require('vm'),assert=require('assert');
-const code=fs.readFileSync('js/core/ticket-data-sync-engine.js','utf8');
-const sandbox={window:{},localStorage:{setItem(){}}};sandbox.window=sandbox;vm.runInNewContext(code,sandbox);
-const E=sandbox.MonsterTicketDataSync;
-assert.equal(E.tokenOf({id:'token25',token:25}),25);
-assert.equal(E.tokenOf({id:'token25',token:0}),0);
-assert.equal(E.normalizeTicket('x',{tokenCount:12}).token,12);
-const n=E.normalizeTicket('x',{token:25,tokenCount:25,coinCount:25});assert.equal(n.token,25);assert.ok(!('tokenCount' in n));
-console.log('V7.7.1 ticket data sync tests passed');
